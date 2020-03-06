7f2e171638d007920b4db87fefc4a41f5db23f5b
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-03-06 12:47:51 +0000

    feat(ci): add prettier and stylelint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-    semi: true,
-    trailingComma: 'none',
-    singleQuote: true,
     printWidth: 120,
     tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'none',
+    jsxBracketSameLine: false,
+    semi: true,
+    arrowParens: 'always'
 };
