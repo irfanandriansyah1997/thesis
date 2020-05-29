81b47d841451e90b7f56413fbf39885cf95dd745
nafhul <nafhul.arsyad@gmail.com>
2020-05-29 13:29:00 +0700

    feat(component): listing inquiry update interface

--- src/desktop-site/organisms/listing-inquiry-r123/interface/component.interface.ts
+++ src/desktop-site/organisms/listing-inquiry-r123/interface/component.interface.ts
@@ -1,40 +1,79 @@
-export interface ListingInquiryAgentPropsInterface {
+/**
+ * Listing Inquiry R123 Agent Info Props Interface
+ * @author Nafhul <nafhul.arsyad@99.co>
+ * @since 2020.05.28
+ */
+export type ListingInquiryAgentPropsInterface = {
     agentName: string;
     agentJoinInfo?: string;
     agentImageUrl: string;
     agentPageUrl: string;
-}
+};
 
+/**
+ * Listing Inquiry R123 Organization Info Props Interface
+ * @author Nafhul <nafhul.arsyad@99.co>
+ * @since 2020.05.28
+ */
 export interface ListingInquiryOrgPropsInterface {
-    organizationName: string;
+    organizationName?: string;
     organizationImageUrl?: string;
-    organizationPageUrl: string;
+    organizationPageUrl?: string;
 }
 
+/**
+ * Listing Inquiry Button R123 Props Interface
+ * @author Nafhul <nafhul.arsyad@99.co>
+ * @since 2020.05.28
+ */
 export interface ListingInquiryButtonPropsInterface {
     buttonText: string;
     buttonIcon?: string;
-    onClickInquiryButton: () => void;
+    onClickInquiryButton: (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => void;
 }
 
+/**
+ * Listing Inquiry R123 Contact Props Interface
+ * @author Nafhul <nafhul.arsyad@99.co>
+ * @since 2020.05.28
+ */
 export interface ListingInquiryContactPropsInterface {
     phoneNumber: string;
-    onClickWhatsAppButton();
-    onClickPhoneButton();
+    onClickWhatsAppButton: (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => void;
+    onClickPhoneButton: (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => void;
 }
 
+/**
+ * Listing Inquiry Button Email Props Interface
+ * @author Nafhul <nafhul.arsyad@99.co>
+ * @since 2020.05.28
+ */
 export interface ListingInquiryMsgPropsInterface {
-    buyerName: string;
-    buyerEmail: string;
-    buyerPhoneNumber: string;
-    message: string;
+    buyerName?: string;
+    buyerEmail?: string;
 }
 
+/**
+ * Listing Inquiry R123 Sidebar Props Interface
+ * @author Nafhul <nafhul.arsyad@99.co>
+ * @since 2020.05.28
+ */
 export type ListingInquirySidebarPropsInterface = ListingInquiryAgentPropsInterface &
     ListingInquiryOrgPropsInterface &
     ListingInquiryContactPropsInterface &
     ListingInquiryButtonPropsInterface;
 
+/**
+ * Listing Inquiry R123 Box Props Interface
+ * @author Nafhul <nafhul.arsyad@99.co>
+ * @since 2020.05.28
+ */
 export type ListingInquiryBoxPropsInterface = ListingInquiryAgentPropsInterface &
     ListingInquiryOrgPropsInterface &
     ListingInquiryContactPropsInterface &
