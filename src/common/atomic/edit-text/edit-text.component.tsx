b5c8071f83a7110b3efa74fa902d8167445d0f4a
cathrine <cathrine.holic@gmail.com>
2020-05-29 15:33:02 +0700

    fix(atomic-edit-text): edit-text fix

--- src/common/atomic/edit-text/edit-text.component.tsx
+++ src/common/atomic/edit-text/edit-text.component.tsx
@@ -1,47 +1,65 @@
-import React, { SFC } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
+import {
+    EditTextDefaultExportInterface,
+    EditTextStyle,
+    EditTextType
+} from './interface/component.interface';
 import StringHelper from '../../../shared/helper/string.helper';
 import ValidatorHelper from '../../../shared/helper/validator.helper';
-import { EditTextPropsInterface } from './interface/component.interface';
+import EditTextAddonComponent from './edit-text-addon.component';
 
 /**
  * Edit Text Component
  * @author Cathrine <Cathrine@99.co>
  * @since 2020.05.20
  */
-const EditTextComponent: SFC<EditTextPropsInterface> = ({
+const EditTextComponent: EditTextDefaultExportInterface = ({
     className,
     value,
+    styling,
     ...res
-}: EditTextPropsInterface) => {
+}) => {
     return (
-        <input
+        <div
             className={StringHelper.objToString({
                 [`ui-atomic-edit-text`]: true,
                 [`${className}`]: ValidatorHelper.verifiedIsNotEmpty(className)
             })}
-            value={value}
-            {...res}
-        />
+        >
+            <input
+                className={StringHelper.objToString({
+                    [`ui-atomic-edit-text__input`]: true,
+                    [`ui-atomic-edit-text--styling-${styling}`]: true
+                })}
+                value={value}
+                {...res}
+            />
+        </div>
     );
 };
 
 EditTextComponent.defaultProps = {
     className: undefined,
     type: 'text',
-    placeholder: undefined,
-    disabled: false,
-    value: ''
+    value: '',
+    styling: 'primary'
 };
 
 EditTextComponent.propTypes = {
     className: PropTypes.string,
-    placeholder: PropTypes.string,
-    type: PropTypes.oneOf(['text', 'number', 'password', 'email']),
+    type: PropTypes.oneOf<EditTextType>([
+        'text',
+        'number',
+        'password',
+        'email'
+    ]),
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    disabled: PropTypes.bool,
-    name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    styling: PropTypes.oneOf<EditTextStyle>(['primary', 'secondary'])
 };
+
+EditTextComponent.Addon = EditTextAddonComponent;
 
 export default EditTextComponent;
